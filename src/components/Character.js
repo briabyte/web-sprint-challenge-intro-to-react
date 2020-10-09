acd2644f1bdfb7578c35aaaefe914baef711488f
bria barry <briabarry@iMac.local>
2020-10-09 08:46:23 -0700

    Finished

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,26 @@
 // Write your Character component here
+
+import React,{useState, useEffect } from "react";
+ import axios from "axios";
+ import Char from "./Char";
+ import styled from "styled-components";
+
+ const Card = styled.div`
+     display:flex;
+
+
+ `;
+
+ const Character = ({name, dob, gender, mass,skin_color,height,hair_color}) =>{
+
+
+     if(!name) return <h2>Loading...</h2>;
+     return (
+         <Char name={name} dob={dob} gender={gender} 
+         mass={mass} skin_color={skin_color} height={height} hair_color={hair_color}
+         />
+     );
+ };
+
+
+ export default Character; 
